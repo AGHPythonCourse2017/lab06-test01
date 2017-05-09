0ab7b9083f46a01ab14ab325552c97692dab6109
Mateusz Woś <wos.mateusz16@gmail.com>
2017-05-09 18:21:24 +0200

    xD

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -11,10 +11,23 @@ class TestDummy(unittest.TestCase):
 
     def test_fun(self):
         weekday = calculate(2001, 1, 3)
-        self.assertEqual(weekday, 2005)
+        self.assertEqual(weekday, 3)
+
+        weekday2 = calculate(2016, 10, 15)
+        self.assertEqual(weekday2, 6)
+
+        weekday3 = calculate(2017, 2, 28)
+        self.assertEqual(weekday3, 1)
+
+        weekday4 = calculate(2017, 2, 29)
+        self.assertNotEqual(weekday4, 2)
 
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
+
+        retcode = main(("--year", "2017", "--month", "2", "--day", "29"))
+        self.assertNotEqual(retcode, 0)
+
 
 
 if __name__ == '__main__':
