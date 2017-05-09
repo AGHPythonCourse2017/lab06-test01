b52b4617a8eded176877e3c22149652d08db3245
Maksymilian Wojczuk <max.wojczuk@appliscale.io>
2017-05-09 18:20:21 +0200

    Added tests for calculate function and main

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -8,13 +8,42 @@ Run with PYTHONPATH=. python tests/test_dummy.py
 
 
 class TestDummy(unittest.TestCase):
+    def test_calculate(self):
+        weekday = calculate(2017, 5, 9)
+        self.assertEqual(weekday, 1)
 
-    def test_fun(self):
-        weekday = calculate(2001, 1, 3)
-        self.assertEqual(weekday, 2005)
+        weekday = calculate(1978, 2, 1)
+        self.assertEqual(weekday, 1)
+
+        weekday = calculate(2000, 7, 9)
+        self.assertEqual(weekday, 6)
+
+        weekday = calculate(2000, 15, 9)
+        self.assertEqual(weekday, -1)
+
+        weekday = calculate(2000, 4, 40)
+        self.assertEqual(weekday, -1)
+
+        weekday = calculate(200012, 15, 9)
+        self.assertEqual(weekday, -1)
 
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
+
+        retcode = main(("--year", "-5", "--month", "10", "--day", "20"))
+        self.assertNotEqual(retcode, 0)
+
+        retcode = main(("--year", "2033", "--month", "2", "--day", "29"))
+        self.assertNotEqual(retcode, 0)
+
+        retcode = main(("--year", "2033", "--month", "2", "--day", "29"))
+        self.assertNotEqual(retcode, 0)
+
+        retcode = main(("--year", "2012", "--month", "13", "--day", "15"))
+        self.assertNotEqual(retcode, 0)
+
+        retcode = main(("--year", "2010", "--month", "11", "--day", "30"))
+        self.assertNotEqual(retcode, 0)
 
 
 if __name__ == '__main__':
