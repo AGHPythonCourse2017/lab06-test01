2c8f384cf43f6bbfb39f626d4e2b52e894ac6153
Patrycja Tarasińska <patrycja.tarasinska@gmail.com>
2017-05-09 18:20:32 +0200

    added tests for calculate

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -10,10 +10,22 @@ Run with PYTHONPATH=. python tests/test_dummy.py
 class TestDummy(unittest.TestCase):
 
     def test_fun(self):
-        weekday = calculate(2001, 1, 3)
-        self.assertEqual(weekday, 2005)
+        # weekday = calculate(2001, 1, 3)
+        # self.assertEqual(weekday, 2005)
+        #
+        # retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
+        # self.assertEqual(retcode, 0)
 
-        retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
+        weekday = calculate(2011, 5, 13)
+        self.assertEqual(weekday, 4)
+
+        weekday = calculate(2016, 2, 29)
+        self.assertEqual(weekday, 0)
+
+        weekday = calculate(2018, 5, 2)
+        self.assertEqual(weekday, 2)
+
+        retcode = main(("--year", "2011", "--month", "5", "--day", "13"))
         self.assertEqual(retcode, 0)
 
 
