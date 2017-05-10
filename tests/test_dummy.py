e98cfc6c37beea4b750d89cc0ad92f5e4efa759a
dykra <asiapalewicz@gmail.com>
2017-05-10 18:38:22 +0200

    few tests added

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -29,12 +29,12 @@ class TestDummy(unittest.TestCase):
         tested_value3 = main(("--year", "2017", "--month", "styczeń", "--day", "12"))
         self.assertNotEquals(tested_value3, 0)
 
+        tested_value3 = main(("--year", "rat Year", "--month", "1", "--day", "12"))
+        self.assertNotEquals(tested_value3, 0)
+
     def test_calculate(self):
         tested_weekday = calculate(2001-2034, 1, 3)
         self.assertEqual(tested_weekday, None)
-
-        tested_weekday = calculate(2017, 1, 3)
-        self.assertEqual(tested_weekday, 1)
 
         tested_weekday = calculate(2017, 1, 0)
         self.assertEqual(tested_weekday, None)
@@ -45,10 +45,19 @@ class TestDummy(unittest.TestCase):
         tested_weekday = calculate(2234, 3, 12)
         self.assertEqual(tested_weekday, None)
 
+        tested_weekday = calculate(2101, "II", 23)
+        self.assertEqual(tested_weekday, None)
+
         tested_weekday = calculate(2101, 1, 19)
         self.assertEqual(tested_weekday, 2)
 
-        tested_weekday = calculate(2101, "II", 23)
+        tested_weekday = calculate(2018, 12, 27)
+        self.assertEqual(tested_weekday, 3)
+
+        tested_weekday = calculate(2017, 1, 3)
+        self.assertEqual(tested_weekday, 1)
+
+        tested_weekday = calculate("300p.n.e.", 12, 3)
         self.assertEqual(tested_weekday, None)
 
         # weekday = calculate(2001, 1, 3)
